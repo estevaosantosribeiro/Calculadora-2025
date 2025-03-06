893ecb5ab5cc8138b49cf527963e70149c201aaf
estevaosantosribeiro <estevaosantosribeiro@gmail.com>
2025-03-06 17:34:10 -0300

    Adicionando subtração

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("--------------------------------");
             Console.WriteLine("Calculadora Tabajara 2025");
+            Console.WriteLine("--------------------------------");
+
+            Console.WriteLine("1 - Somar");
+            Console.WriteLine("2 - Subtrair");
+            Console.WriteLine("S - Sair");
+
+            Console.Write("Escolha uma opção: ");
+
+            string opcao = Console.ReadLine().ToUpper();
+
+            if (opcao == "S") {
+                return;
+            }
 
             Console.Write("Digite o primeiro número: ");
             string primeiroNumeroString = Console.ReadLine();
@@ -14,7 +28,13 @@
             string segundoNumeroString = Console.ReadLine();
             int segundoNumero = Convert.ToInt32(segundoNumeroString);
 
-            int resultado = primeiroNumero + segundoNumero;
+            int resultado = 0;
+
+            if (opcao == "1") {
+                resultado = primeiroNumero + segundoNumero;
+            } else {
+                resultado = primeiroNumero - segundoNumero;
+            }
 
             Console.WriteLine(resultado);
 
