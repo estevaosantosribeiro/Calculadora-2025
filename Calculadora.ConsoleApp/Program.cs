f95c5bda192a7d1e1e83536a1f1a03d097b0dbeb
estevaosantosribeiro <estevaosantosribeiro@gmail.com>
2025-03-06 17:06:43 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora.ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Calculadora.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora Tabajara 2025");
+
+            Console.Write("Digite o primeiro número: ");
+            string primeiroNumeroString = Console.ReadLine();
+            int primeiroNumero = Convert.ToInt32(primeiroNumeroString);
+
+            Console.Write("Digite o segundo número: ");
+            string segundoNumeroString = Console.ReadLine();
+            int segundoNumero = Convert.ToInt32(segundoNumeroString);
+
+            int resultado = primeiroNumero + segundoNumero;
+
+            Console.WriteLine(resultado);
+
+            Console.ReadLine();
+        }
+    }
+}
