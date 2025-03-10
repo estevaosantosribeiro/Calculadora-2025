051dc9d5e7fdf8c44dca2bc2ed8e4bff05b157cf
estevaosantosribeiro <estevaosantosribeiro@gmail.com>
2025-03-10 16:11:46 -0300

    Adicionada funcionalidade de execução em loop

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -4,41 +4,55 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("--------------------------------");
-            Console.WriteLine("Calculadora Tabajara 2025");
-            Console.WriteLine("--------------------------------");
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine("Calculadora Tabajara 2025");
+                Console.WriteLine("--------------------------------");
 
-            Console.WriteLine("1 - Somar");
-            Console.WriteLine("2 - Subtrair");
-            Console.WriteLine("S - Sair");
+                Console.WriteLine("1 - Somar");
+                Console.WriteLine("2 - Subtrair");
+                Console.WriteLine("S - Sair");
 
-            Console.Write("Escolha uma opção: ");
+                Console.Write("Escolha uma opção: ");
 
-            string opcao = Console.ReadLine().ToUpper();
+                string opcao = Console.ReadLine().ToUpper();
 
-            if (opcao == "S") {
-                return;
+                if (opcao == "S")
+                    break;
+
+                Console.Write("Digite o primeiro número: ");
+                string primeiroNumeroString = Console.ReadLine();
+                decimal primeiroNumero = Convert.ToDecimal(primeiroNumeroString);
+
+                Console.Write("Digite o segundo número: ");
+                string segundoNumeroString = Console.ReadLine();
+                decimal segundoNumero = Convert.ToInt32(segundoNumeroString);
+
+                decimal resultado = 0;
+
+                if (opcao == "1")
+                {
+                    resultado = primeiroNumero + segundoNumero;
+                }
+                else if (opcao == "2")
+                {
+                    resultado = primeiroNumero - segundoNumero;
+                }
+
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine("Resultado: " + resultado);
+                Console.WriteLine("--------------------------------");
+
+                Console.WriteLine("Deseja continuar? (S/N): ");
+                string opcaoContinuar = Console.ReadLine().ToUpper();
+
+                if (opcaoContinuar != "S")
+                    break;
+
+                Console.ReadLine();
             }
-
-            Console.Write("Digite o primeiro número: ");
-            string primeiroNumeroString = Console.ReadLine();
-            int primeiroNumero = Convert.ToInt32(primeiroNumeroString);
-
-            Console.Write("Digite o segundo número: ");
-            string segundoNumeroString = Console.ReadLine();
-            int segundoNumero = Convert.ToInt32(segundoNumeroString);
-
-            int resultado = 0;
-
-            if (opcao == "1") {
-                resultado = primeiroNumero + segundoNumero;
-            } else {
-                resultado = primeiroNumero - segundoNumero;
-            }
-
-            Console.WriteLine(resultado);
-
-            Console.ReadLine();
         }
     }
 }
